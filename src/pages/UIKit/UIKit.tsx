73f0b3f3d1e5af6cb23df5a7530f371430294596
ForestSpirit <forestspirit@email.cz>
2025-12-06 12:38:17 +0200

    add: input component

--- src/pages/UIKit/UIKit.tsx
+++ src/pages/UIKit/UIKit.tsx
@@ -1,11 +1,32 @@
-import './UIKit.scss';
+import { useState } from "react";
+import { Input } from "../../components/ui/Input";
+import "./UIKit.scss";
 
 function UIKit() {
+  const [inputValue, setInputValue] = useState("");
+  // console.log(inputValue);
   return (
     <div className="ui-kit">
       <h1>UI Kit</h1>
-      <p>Сторінка для перегляду UI компонентів</p>
-      
+      <p className="ui-kit__description">
+        Сторінка для перегляду UI компонентів
+      </p>
+
+      <section className="ui-kit__section">
+        <h2>Input</h2>
+        <div className="ui-kit__inputs">
+          <Input
+            label="Default"
+            placeholder="Введіть текст..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Input label="Small" placeholder="Small input" size="sm" />
+          <Input label="Large" placeholder="Large input" size="lg" />
+          <Input label="Disabled" placeholder="Disabled input" disabled />
+        </div>
+      </section>
+
       <section className="ui-kit__section">
         <h2>Типографіка</h2>
         <h1>Заголовок H1</h1>
@@ -13,8 +34,6 @@ function UIKit() {
         <h3>Заголовок H3</h3>
         <h4>Заголовок H4</h4>
         <p>Звичайний текст параграфа. Lorem ipsum dolor sit amet.</p>
-        <p className="text-muted">Приглушений текст</p>
-        <a href="#">Посилання</a>
       </section>
 
       <section className="ui-kit__section">
@@ -27,14 +46,8 @@ function UIKit() {
           <div className="ui-kit__color ui-kit__color--info">Info</div>
         </div>
       </section>
-
-      <section className="ui-kit__section">
-        <h2>Компоненти</h2>
-        <p>Тут будуть додаватись UI компоненти...</p>
-      </section>
     </div>
   );
 }
 
 export default UIKit;
-
