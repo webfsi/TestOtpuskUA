186ae2b6e05cc7bf444725fbe9722c1a7c03dae2
ForestSpirit <forestspirit@email.cz>
2025-12-06 18:13:13 +0200

    feat: integrate tour cards

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { SearchForm } from "../../features/SearchForm";
 import { SearchResults } from "../../features/SearchResults";
-import { useSearchPrices } from "../../hooks";
+import { useSearchPrices, useHotels } from "../../hooks";
 import { DropdownItem } from "../../components/ui/Dropdown";
+import { TourCard } from "../../components/ui/TourCard";
+import { formatDate } from "../../utils";
 import "./Home.scss";
 
 function Home() {
   const { isLoading, error, data, search, abort } = useSearchPrices();
+  const { data: hotels, fetch: fetchHotels } = useHotels();
+  const [countryId, setCountryId] = useState<string | null>(null);
 
   useEffect(() => {
     return () => {
@@ -14,14 +18,42 @@ function Home() {
     };
   }, [abort]);
 
+  useEffect(() => {
+    if (data && countryId) {
+      fetchHotels(countryId);
+    }
+  }, [data, countryId, fetchHotels]);
+
   const handleSearch = (item: DropdownItem) => {
     if (item.type === "country") {
+      setCountryId(item.id);
       search(item.id);
     }
   };
 
   const isEmpty = data && Object.keys(data).length === 0;
-  const hasData = data && Object.keys(data).length > 0;
+  const hasData = data && Object.keys(data).length > 0 && hotels;
+
+  const tourCards = hasData
+    ? Object.values(data).map((price) => {
+        const hotel = hotels[price.hotelID];
+        if (!hotel) return null;
+
+        return (
+          <TourCard
+            key={price.id}
+            hotelName={hotel.name}
+            hotelImage={hotel.img}
+            cityName={hotel.cityName}
+            countryName={hotel.countryName}
+            startDate={formatDate(price.startDate)}
+            price={price.amount}
+            currency={price.currency === "usd" ? "$" : "грн"}
+            priceId={price.id}
+          />
+        );
+      })
+    : null;
 
   return (
     <div className="home">
@@ -33,12 +65,7 @@ function Home() {
         isEmpty={isEmpty}
         emptyText="За вашим запитом турів не знайдено"
       >
-        {hasData && (
-          <div className="home__results">
-            <p>Знайдено {Object.keys(data).length} пропозицій</p>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-          </div>
-        )}
+        {tourCards && <div className="home__cards">{tourCards}</div>}
       </SearchResults>
     </div>
   );
